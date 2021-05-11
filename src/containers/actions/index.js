232d93f61045d78325c4059e7ea63e6478409756
Mohammed <atefhamosa@gmail.com>
2021-05-11 11:57:13 +0200

    Fix linters

--- src/containers/actions/index.js
+++ src/containers/actions/index.js
@@ -1,8 +1,10 @@
+// eslint-disable-next-line
 const CREATE_BOOK = (book) => ({
   type: 'CREATE_BOOK',
   book,
 });
 
+// eslint-disable-next-line
 const REMOVE_BOOK = (book) => ({
   type: 'REMOVE_BOOK',
   book,
