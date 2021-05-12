b6f9f3f75b66f9d81635458b66e8cf64c13a5905
Mohammed <atefhamosa@gmail.com>
2021-05-12 10:38:12 +0200

    Style book conponent

--- src/components/Book.js
+++ src/components/Book.js
@@ -7,27 +7,47 @@ const Book = (props) => {
   } = props;
 
   return (
-    <div className="book">
-      <p className="b-category">{category}</p>
-      <p className="b-title">{title}</p>
-      <p className="f-blue">Mohamed Atef</p>
-      <div className="btns">
-        <button type="button" className="f-blue btn">
-          Comments
-        </button>
-        |
-        <button
-          className="f-blue btn"
-          type="button"
-          value={id}
-          onClick={handleClick}
-        >
-          Remove
-        </button>
-        |
-        <button type="button" className="f-blue btn">
-          Edit
-        </button>
+    <div className="book flex-row">
+      <div className="w-33">
+        <p className="b-category">{category}</p>
+        <p className="b-title">{title}</p>
+        <p className="f-blue">Mohamed Atef</p>
+        <div className="btns">
+          <button type="button" className="f-blue btn">
+            Comments
+          </button>
+          |
+          <button
+            className="f-blue btn"
+            type="button"
+            value={id}
+            onClick={handleClick}
+          >
+            Remove
+          </button>
+          |
+          <button type="button" className="f-blue btn">
+            Edit
+          </button>
+        </div>
+      </div>
+      <div className="flex-row w-66">
+        <div className="flex-row completed">
+          <div>
+            <div className="circle"> </div>
+          </div>
+          <div>
+            <h2>75%</h2>
+            <p className="b-category">Completed</p>
+          </div>
+        </div>
+        <div className="chapter">
+          <p className="b-category">Current Chapter</p>
+          <h6>Chapter 17</h6>
+          <button type="button" className="update-btn">
+            UPDATE PROGRESS
+          </button>
+        </div>
       </div>
     </div>
   );
